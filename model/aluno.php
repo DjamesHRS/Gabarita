0c8d87630c25366c03c87d835a9d01b8674c31bd
DjamesHRS <djamesrenunza@gmail.com>
2025-09-02 09:14:39 -0300

    Adição de itens

---
+++ model/aluno.php
@@ -0,0 +1,11 @@
+<?php
+    class Aluno{
+        public $id;
+        public $nome;
+        public $cpf;
+        public $numero_de_matricula;
+        public $email;
+        public $sexo;
+        public $telefone;
+    }
+?>
